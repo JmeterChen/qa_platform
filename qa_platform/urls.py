959918724708945f78630911ca48f5caf026df9e
zhengyanqing <zhengyanqing@fangdd.com>
2020-08-26 14:27:32 +0800

    删除多余url
    Change-Id: Icf8ab8a5b263ffaabf62807337f436868760d755

--- qa_platform/urls.py
+++ qa_platform/urls.py
@@ -6,10 +6,9 @@ urlpatterns = [
     path('index/', include('index.urls')),
     path('tapd/', include('tapd.urls')),
     path('api/v1/problem_guide/', include('problem_guide.urls')),
-    path('user/', include('user.urls')),
+    path('api/v1/user/', include('user.urls')),
     re_path('^api/v1/', include("mypro.urls")),
     re_path('^api/v1/', include('qualitydata.urls')),
     re_path('^api/v1/', include("testcase.urls")),
-    re_path('^api/v1/', include("user.urls")),
     re_path('^api/v1/', include("tapd.urls")),
 ]
