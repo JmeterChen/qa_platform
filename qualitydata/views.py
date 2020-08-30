a72ca7f8132039637a2c471d43ad1c8908a734db
luoyuan <luoyuan@fangdd.com>
2020-08-31 00:35:43 +0800

    update
    Change-Id: I444d9a679ef3e46f68e1504eb5ec5766ad577a51

--- qualitydata/views.py
+++ qualitydata/views.py
@@ -1,12 +1,13 @@
 from django.views import View
-from mypro.models import Iterable, OnlineBug, App, Project, User
+from mypro.models import Iterable, OnlineBug, App, Project
 from django.http import JsonResponse
 import json
 from django.core import serializers
 from django.core.paginator import Paginator
 import time
 import datetime
-from django.db.models import Q, Sum, Count
+from django.db.models import Q, Sum
+import calendar
 
 default_pageNo = 1
 default_pageSize = 10
@@ -17,32 +18,49 @@ class IterableView(View):
     def get(self, request, *args, **kwargs):
         req = request.GET
         conditions = {}
-        stat_time_condition1 = {}
-        stat_time_condition2 = {}
+        conditions['is_delete'] = 0
+        page_size, page_no = req.get("page_size", default_pageSize), req.get("page_no", default_pageNo)
         if req.get('project_id'):
             conditions['project_id'] = req.get('project_id')
         if req.get('product_id'):
             conditions['product_id'] = req.get('product_id')
-        if req.get('start_time') and req.get('end_time'):
-            stat_time_condition1['start_time__gte'] = req.get('start_time')
-            stat_time_condition1['start_time__lte'] = req.get('end_time')
-            stat_time_condition2['end_time__gte'] = req.get('start_time')
-            stat_time_condition2['end_time__lte'] = req.get('end_time')
         if req.get('create_start_time'):
-            conditions['create_time__gte'] = req.get('create_start_time')
+            conditions['create_time__gte'] = datetime.datetime.strptime(req.get('create_start_time'), '%Y-%m-%d')
         if req.get('create_end_time'):
-            conditions['create_time__lte'] = req.get('create_end_time')
-        conditions['is_delete'] = 0
-        page_size, page_no = req.get("page_size", default_pageSize), req.get("page_no", default_pageNo)
+            conditions['create_time__lte'] = datetime.datetime.strptime(req.get('create_end_time'), '%Y-%m-%d') + datetime.timedelta(days=1)
+        if req.get('type') == '1':
+            if req.get('start_time'):
+                conditions['end_time__gte'] = req.get('start_time')
+            if req.get('end_time'):
+                conditions['start_time__lte'] = req.get('end_time')
+            start_time = datetime.datetime.strptime(req.get('start_time'), '%Y-%m-%d')
+            end_time = datetime.datetime.strptime(req.get('end_time'), '%Y-%m-%d')
+            db_data = Iterable.objects.filter(**conditions).values('product_id', 'project_id', 'publish_num', 'cases_num', 'bugs_num', 'start_time', 'end_time')
+            for data in db_data:
+                if data['start_time'] < start_time and data['end_time'] < end_time and (data['end_time'] - start_time).days < (start_time - data['start_time']).days:
+                    db_data.remove(data)
+                if data['start_time'] > start_time and data['start_time'] < end_time and data['end_time'] > end_time and (data['end_time'] - end_time).days > (end_time - data['start_time']).days:
+                    db_data.remove(data)
+                if data['start_time'] < start_time and data['end_time'] > end_time and (data['end_time'] - data['start_time']).days > (end_time-start_time)*2:
+                    db_data.remove(data)
 
-        db_data = Iterable.objects.filter(
-            (Q(**stat_time_condition1) | Q(**stat_time_condition2)) & Q(**conditions)).values('product_id',
-                                                                                              'project_id').annotate(
-            publish_num=Sum('publish_num'), cases_num=Sum('cases_num'), bugs_num=Sum('bugs_num'))
+        if req.get('type') == '2':
+            if req.get('start_time'):
+                conditions['year__gte'] = datetime.datetime.strptime(req.get('start_time'), '%Y-%m-%d').year
+                conditions['month__gte'] = datetime.datetime.strptime(req.get('start_time'), '%Y-%m-%d').month
+            if req.get('end_time'):
+                conditions['year__lte'] = datetime.datetime.strptime(req.get('end_time'), '%Y-%m-%d').year
+                conditions['month__lte'] = datetime.datetime.strptime(req.get('end_time'), '%Y-%m-%d').month
+            db_data = Iterable.objects.filter(**conditions).values('product_id', 'project_id').annotate(
+                publish_num=Sum('publish_num'), cases_num=Sum('cases_num'), bugs_num=Sum('bugs_num'))
         page_data = Paginator(db_data, page_size).get_page(page_no)
         total = Paginator(db_data, default_pageSize).count
-        project_id_list = [i['project_id'] for i in page_data.object_list.values('project_id')]
-        product_id_list = [i['product_id'] for i in page_data.object_list.values('product_id')]
+        if req.get('type') == '1':
+            project_id_list = [i['project_id'] for i in page_data.object_list]
+            product_id_list = [i['product_id'] for i in page_data.object_list]
+        if req.get('type') == '2':
+            project_id_list = [i['project_id'] for i in page_data.object_list.values('project_id')]
+            product_id_list = [i['product_id'] for i in page_data.object_list.values('product_id')]
         # 查询product信息
         product_info_list = App.objects.filter(product_id__in=product_id_list).values('product_id', 'product_name')
         # 查询项目信息
@@ -54,16 +72,16 @@ class IterableView(View):
         create_time_map = {}
         op_user_map = {}
         for data in base_info_list:
-
-            if not stat_start_time_map.keys().__contains__(data.project_id) or stat_start_time_map[data.project_id] > data.start_time:
+            if not stat_start_time_map.keys().__contains__(data.project_id) or stat_start_time_map[
+                data.project_id] > data.start_time:
                 stat_start_time_map[data.project_id] = data.start_time
-            if not stat_end_time_map.keys().__contains__(data.project_id) or stat_end_time_map[data.project_id] < data.end_time:
+            if not stat_end_time_map.keys().__contains__(data.project_id) or stat_end_time_map[
+                data.project_id] < data.end_time:
                 stat_end_time_map[data.project_id] = data.end_time
             if not create_time_map.keys().__contains__(data.project_id):
                 create_time_map[data.project_id] = data.create_time
             if not op_user_map.keys().__contains__(data.project_id):
                 op_user_map[data.project_id] = data.op_user_name
-
 
         product_info_map = {i['product_id']: i['product_name'] for i in product_info_list}
         project_info_map = {i['project_id']: i['project_name'] for i in project_info_list}
@@ -96,12 +114,20 @@ class IterableView(View):
                 product_id_values = App.objects.filter(product_id=product_id).first()
                 project_id_values = Project.objects.filter(product_id=product_id, project_id=project_id)
                 if project_id_values:
+                    pre_day = calendar.monthrange(int(start_time[0:4]), int(start_time[5:7]))[1] - int(start_time[-2:])
+                    last_day = int(end_time[-2:]) - 1
+                    if pre_day >= last_day:
+                        year = int(start_time[0:4])
+                        month = int(start_time[5:7])
+                    else:
+                        year = int(end_time[0:4])
+                        month = int(end_time[5:7])
                     try:
                         db_data = {"product_id": product_id, "project_id": project_id,
                                    "publish_num": publish_num, "cases_num": cases_num, "end_time": end_time,
                                    "op_user_name": op_user_name,
                                    "start_time": start_time, "bugs_num": bugs_num, "test_user_id": test_user_id,
-                                   "create_time": time, "update_time": time, "is_delete": 0}
+                                   "create_time": time, "update_time": time, 'year':year, 'month':month, "is_delete": 0}
                         data = Iterable.objects.create(**db_data)
                         data.save()
                         res = {"code": 20000, "success": True, "msg": "添加成功！", "data": db_data}
@@ -132,6 +158,15 @@ class IterableView(View):
                             "test_user_id"), req_data.get("start_time"), req_data.get("end_time")
                         op_user_name = request.META.get("HTTP_NAME", '')
                         if product_id and project_id and publish_num and cases_num and bugs_num and test_user_id and start_time and end_time and op_user_name:
+                            pre_day = calendar.monthrange(int(start_time[0:4]), int(start_time[5:7]))[1] - int(
+                                start_time[-2:])
+                            last_day = int(end_time[-2:]) - 1
+                            if pre_day >= last_day:
+                                year = int(start_time[0:4])
+                                month = int(start_time[5:7])
+                            else:
+                                year = int(end_time[0:4])
+                                month = int(end_time[5:7])
                             try:
                                 Iterable.objects.filter(id=req_id).update(product_id=product_id, project_id=project_id,
                                                                           publish_num=publish_num,
@@ -139,7 +174,7 @@ class IterableView(View):
                                                                           test_user_id=test_user_id,
                                                                           start_time=start_time, end_time=end_time,
                                                                           op_user_name=op_user_name,
-                                                                          update_time=time)
+                                                                          update_time=time, year=year,month=month)
                                 res = {"code": 20000, "success": True, "msg": "编辑成功！", "data": req_data}
                             except Exception as e:
                                 res = {"code": 10014, "success": False, "msg": "编辑失败！", "data": e}
