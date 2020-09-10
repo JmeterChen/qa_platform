d431aa80f7465bd2aca5f807b7156793e8d031c6
chenwenjian <chenwenjian@fangdd.com>
2020-09-10 17:33:58 +0800

    add bugly tasks
    Change-Id: I0ca7e1c590f33d2bde49f54ae13818f15a65a064

--- bugly/models.py
+++ bugly/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BuglyData(models.Model):
+	eventType = models.CharField("事件", null=False, max_length=50)
+	timestamp = models.CharField("发布该数据时间搓", null=False, max_length=50)
+	date = models.CharField("数据收集日期", null=False, max_length=10)
+	appName = models.CharField('APP名称', null=False, max_length=20)
+	appId = models.CharField('AppID', null=False, max_length=20)
+	appUrl = models.URLField("数据查询链接",  null=False, max_length=200)
+	version = models.CharField("APP版本",  null=False, max_length=50)
+	platformId = models.IntegerField("APP平台", null=False, default=1)
+	signature = models.CharField(null=False, max_length=50)
+	accessCount = models.IntegerField("联网总次数", null=False)
+	accessUser = models.IntegerField("联网总人数", null=False)
+	crashCount = models.IntegerField("奔溃总次数", null=False)
+	crashUser = models.IntegerField("奔溃总人数", null=False)
+	url = models.URLField("该条数据查询链接",  null=False, max_length=200)
+	create_time = models.DateTimeField('创建时间', auto_now_add=True)
+	is_delete = models.IntegerField('删除状态', choices=((0, '否'), (1, '是')), default=0)
+
+	class Meta:
+		db_table = 'qa_bugly'
--- bugly/views.py
+++ bugly/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 import json
 from mypro.common.func import ding_push
 # Create your views here.
+from bugly.models import BuglyData
+from bugly import tasks
 
 
 def bugly_data(request):
 	# return HttpResponse("Hello world!")
-	req_data = json.loads(request.body)
-	return HttpResponse(ding_push(req_data))
+	try:
+		req_data = json.loads(request.body)
+		data_to_db = []
+		row_data = req_data["eventContent"]["datas"]
+		if row_data:
+			for i in row_data:
+				row = i.copy()
+				row.update({"eventType": req_data.get("eventType"), "timestamp": req_data.get("timestamp"),
+				            "signature": req_data.get("signature"),
+				            "date": req_data.get("eventContent", {}).get("date"),
+				            "appName": req_data.get("eventContent", {}).get("appName"),
+				            "appId": req_data.get("eventContent", {}).get("appId"),
+				            "appUrl": req_data.get("eventContent", {}).get("appUrl"),
+				            "platformId": req_data.get("eventContent", {}).get("platformId")
+				            })
+				# print(row_data.index(i), row)
+				data_to_db.append(row)
+				# BuglyData.objects.create(**row)
+		tasks.saveBuglyData.delay(data_to_db)
+		res = {"code": 10000, "msg": "输入写入成功！", "success": True}
+	except Exception as e:
+		res = {"code": 90000, "msg": e, "success": False}
+	return JsonResponse(res)
